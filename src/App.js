8083bd69eebd589fb8586d2771aaed67077f29ce
Max Solomkin <solomkinmaks8@gmail.com>
2021-05-21 12:12:49 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import axios from 'axios';
+  
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+
+import { Nav, NavItem, NavLink, Container } from 'reactstrap';
+
+import StudentsList from './components/StudentsList';
+import PageAddStudents from './components/PageAddStudents';
+  
+class App extends React.Component {
+    state = {
+        students: null
+    }
+
+    async componentDidMount () {
+        const response = await axios.get('https://my-json-server.typicode.com/Max-jun-source/students-list/Students');
+
+        this.setState({ students: response.data });
+    }
+
+    addNewStudent = (student) => {
+        this.setState({ students: [...this.state.students, student ]});
+        
+    }
+
+    deleteStudent = (id) => {
+        this.state.students.splice(id, 1);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Container>
+                    <Nav>
+                        <NavItem>
+                            <NavLink className="link" href="/">Students List</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="link" href="/add-student">Add Student</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <Switch>
+                        <Route path="/add-student">
+                            <PageAddStudents onClick={this.addNewStudent} />
+                        </Route>
+                        <Route path="/">
+                            <StudentsList student={this.state.students} onClick={this.deleteStudent} />
+                        </Route>
+                    </Switch>
+                </Container>
+            </Router>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/StudentsList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { Table, Button } from 'reactstrap';
+
+class StudentsList extends React.Component {
+    deleteStudent = (id) => {
+        this.props.onClick(id);
+    }
+
+    render () {
+        return (
+            <Table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>University</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {(this.props.student || []).map((student, index) => {
+                        return (
+                            <tr>
+                                <th scope="row">{index+1}</th>
+                                <td>{student.name}</td>
+                                <td>{student.surname}</td>
+                                <td>{student.university}</td>
+                                <td>
+                                    <Button color="danger" size="sm" style={{marginRight: '10px'}}>Edit</Button>
+                                    <Button onClick={() => this.deleteStudent(index)} color="danger" size="sm">Delete</Button>
+                                </td>
+                            </tr>
+                        );
+                    })} 
+                </tbody>
+            </Table>
+        );
+    }
+}
+
+export default StudentsList;
